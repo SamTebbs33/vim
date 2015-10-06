df84c971a16627f3d1209a74664c95e98eec35a9
Sam Tebbs <samuel.tebbs@yahoo.com>
2015-10-06 10:35:04 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 mv .vimrc ~/.vimrc
+rm -f .netwrhist
 echo "let g:netrw_dirhistmax  =10" >> .netrwhist
 echo "let g:netrw_dirhist_cnt =1" >> .netrwhist
 echo "let g:netrw_dirhist_1='$(pwd)'" >> .netrwhist
