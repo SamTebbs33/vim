ee5fbb534fd63215885534ab55df65e1e0dd01f9
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-06 16:21:36 +0100

    Changed setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 mv .vimrc ~/.vimrc
 echo "let g:netrw_dirhistmax  =10" >> .netrwhist
 echo "let g:netrw_dirhist_cnt =1" >> .netrwhist
-
-echo "let g:netrw_dirhist_1=''" >> .netrwhist
+echo "let g:netrw_dirhist_1=$(pwd)" >> .netrwhist
